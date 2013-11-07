81287153c45cb36abbef361a53b2a8aa3b98c134
Shadae Holmes <ShadaeHolmes@gmail.com>
2013-11-07 11:07:28 -0800

    Change to bottlesong

--- first_week/bottlesong.rb
+++ first_week/bottlesong.rb
@@ -1,9 +1,11 @@
-(1..99).to_a.reverse.each. do |number|
-  puts number.to_s +" bottles of beer on the wall.. "
-  puts number.to_s +" bottles of beer on the wall.. "
-  puts " Take one down, pass it around...."
-  number_left = number - 1
+require 'colorize'
 
-  puts number_left.to_s + "bottles of beer on the wall!"
-  puts "-----------"
+(1..99).to_a.reverse.each do |number|
+puts number.to_s + " bottles of beer on the wall.. ".light_cyan
+puts number.to_s + " bottles of beer .. ".light_blue
+puts " Take one down, pass it around....".white.on_light_blue
+number_left = number - 1
+
+puts number_left.to_s + " bottles of beer on the wall!".magenta
+puts "-----------"
 end
